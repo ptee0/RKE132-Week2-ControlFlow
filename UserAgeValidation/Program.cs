321cf53f82ceedde3a45709f84fd04099a1887b2
ptee0 <priit.teearu@gmail.com>
2023-05-18 23:03:30 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi]" / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please select your gender (m/f):"); //CW 2xtab = Console writeline
+
+char userGender = char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis, parsime pika sõne väikseks sümboliks char
+
+Console.WriteLine("Please, enter your last name:"); //kasutajanimi peab olema enne if blocki, sest koodi loetakse ülalt alla ja local variable (userLastName) peab olema enne deklareeritud
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')// == tähendab võrdlemist, = ei tohi olla, sest see salvestab userGender sisse uue väärtuse; " " on stringidel, ' ' on charidel
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')// else if parem, sest kui kasutaja sisestab m kontrollib if ikkagi, kas kasutaja f, kuigi on teada, et kasutaja m
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal sisestada tema vanuse
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+//"You are too young to use Instagram"
+//muul juhul
+//konsoolis kuvatakse "Welcome to Instagram"
+
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine()); // "13" - otsib heapist, 13 - ilma jutumärkideta salvestatakse stacki, seega oleks vaja 13 salvestada arvuna => vaja salvestada "int" tüüpi muutuja sisse; Kuna Console.ReadLine tagastab ainult stringi, nii et vaja parse
+
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
+
+//kui Int32.Parse(Console.ReadLine()) asemel oleks lihtsalt Console.ReadLine(), siis võrdleks teksti numbriga
